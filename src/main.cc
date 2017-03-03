18ed013ef2857fe99be95abb41920104bda4c4f4
Laura Catano <lllaura@cs.utexas.edu>
2017-03-03 15:51:30 -0600

    Updated with geometry shader

--- src/main.cc
+++ src/main.cc
@@ -49,6 +49,27 @@ void main()
 }
 )zzz";
 
+const char* geometry_shader =
+R"zzz(#version 330 core
+layout (triangles) in;
+layout (triangle_strip, max_vertices = 3) out;
+uniform mat4 projection;
+in vec4 vs_light_direction[];
+flat out vec4 normal;
+out vec4 light_direction;
+void main()
+{
+	int n = 0;
+	normal = vec4(0.0, 0.0, 1.0f, 0.0);
+	for (n = 0; n < gl_in.length(); n++) {
+		light_direction = vs_light_direction[n];
+		gl_Position = projection * gl_in[n].gl_Position;
+		EmitVertex();
+	}
+	EndPrimitive();
+}
+)zzz";
+
 const char* fragment_shader =
 R"zzz(#version 330 core
 in vec4 normal;
