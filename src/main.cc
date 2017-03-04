df11912ce492e35782dc675dd9bf21e199b422e7
Sihai Tang <sihai@cs.utexas.edu>
2017-03-03 20:22:17 -0600

    Small color change

--- src/main.cc
+++ src/main.cc
@@ -79,7 +79,7 @@ in vec4 light_direction;
 out vec4 fragment_color;
 void main()
 {
-	vec4 color = vec4(1.0, 0.0, 0.0, 1.0);
+	vec4 color = vec4(0.0, 0.5, 0.2, 1.0);
 	float dot_nl = dot(normalize(light_direction), normalize(normal));
 	dot_nl = clamp(dot_nl, 0.0, 1.0);
 	fragment_color = clamp(dot_nl * color, 0.0, 1.0);
